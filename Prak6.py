cabbdb6f832de9a5f67867f5a4e6223929d5ae7c
Abdullah Hudzafah <96789420+abdulbedul13@users.noreply.github.com>
2021-12-29 01:13:21 +0700

    Create Prak6.py

---
+++ Prak6.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 28 19:23:44 2021
+
+@author: Abdullah
+"""
+
+# Elemen kompetensi 1
+print("MENGHITUNG JARAK TEMPUH GLBB")
+kecepatan_awal = int(input("Masukkan v0: "))
+waktu = int(input("Masukkan t: "))
+percepatan = int(input("Masukkan a: "))
+
+jarak = kecepatan_awal * waktu + 0.5 * percepatan * waktu ** 2
+
+print("Jarak tempuh jika kecepatan awal =  {}, waktu = {} dan percepatan = {} adalah {}".format(kecepatan_awal, waktu, percepatan, jarak))
