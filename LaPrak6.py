9d752dcf66ca73edd03d150ed078fd98cebd7c44
Abdullah Hudzafah <96789420+abdulbedul13@users.noreply.github.com>
2021-12-30 21:43:51 +0700

    Update LaPrak6.py

--- LaPrak6.py
+++ LaPrak6.py
@@ -85,4 +85,4 @@ while True:
         print("TERIMA KASIH!")
         break
     else:
-        print("Pilih 1, 2, 3, 4 atau 5, 6 untuk keluar\n")
+        print("Pilih 1, 2, 3, 4, 5 atau 6 untuk keluar\n")
